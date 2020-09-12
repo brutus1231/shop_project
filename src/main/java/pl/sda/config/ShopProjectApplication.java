a426c52cd572da0b2b4e315f35d654dde2cfb5bf
zdzislaw trzaska <ztrzaska3@gmail.com>
2020-09-12 13:51:00 +0200

    security configuration

--- src/main/java/pl/sda/config/ShopProjectApplication.java
+++ src/main/java/pl/sda/config/ShopProjectApplication.java
@@ -3,8 +3,10 @@ package pl.sda.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @ComponentScan("pl.sda")
+@Import(SecurityConfiguration.class)
 @SpringBootApplication
 public class ShopProjectApplication {
 
