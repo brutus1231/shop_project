8016d6308417119077b57bc1527e37b621998f75
zdzislaw trzaska <ztrzaska3@gmail.com>
2020-09-12 14:05:34 +0200

    security configuration

--- src/main/java/pl/sda/config/SecurityConfiguration.java
+++ src/main/java/pl/sda/config/SecurityConfiguration.java
@@ -3,6 +3,8 @@ package pl.sda.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,6 +22,39 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .withUser("user2").password(passwordEncoder().encode("user2")).roles("USER")
                 .and()
                 .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .antMatchers("/dba/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_DBA')")
+                .antMatchers("/login*").permitAll()
+                .antMatchers("/header*").permitAll()
+                .antMatchers("/regist*").permitAll()
+                .anyRequest().authenticated();
+        http.formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/appLogin")
+                .usernameParameter("username")
+                .passwordParameter("pass")
+                .defaultSuccessUrl("/", true)
+        ;
+        // .failureHandler(authenticationFailureHandler())
+        http.csrf().disable()
+                .headers().frameOptions().disable();
+        http.logout()
+                .logoutSuccessUrl("/login")
+                .logoutUrl("/logout")
+        //  .logoutSuccessHandler(logoutSuccessHandler())
+        ;
     }
 
     @Bean
