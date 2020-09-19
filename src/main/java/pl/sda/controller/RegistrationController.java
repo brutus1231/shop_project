674c63a6261281c1fc19f3173bda196441a084a8
zdzislaw trzaska <ztrzaska3@gmail.com>
2020-09-19 14:43:13 +0200

    registration

--- src/main/java/pl/sda/controller/RegistrationController.java
+++ src/main/java/pl/sda/controller/RegistrationController.java
@@ -2,7 +2,9 @@ package pl.sda.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.sda.dto.UserDto;
 
@@ -16,7 +18,9 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String saveUser(Model model) {
+    public String saveUser(@ModelAttribute(name = "user") UserDto user, BindingResult bindingResult,
+                           Model model) {
+
         return "registration";
     }
 }
